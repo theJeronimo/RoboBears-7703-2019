69b3f98e878dea9c143749994a17b379d3ab963d
jeronartest <jeronsmail@gmail.com>
2019-01-30 16:23:10 -0700

    SpeedControllerGroup and Victors added

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -7,7 +7,12 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import frc.robot.RobotMap;
 
 /**
  * Add your docs here.
@@ -15,6 +20,22 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class DriveSubsystem extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
+
+
+  // Motor Controller Objects
+  public Victor leftMaster = new Victor(RobotMap.leftMasterPort);
+  public Victor leftSlave = new Victor(RobotMap.leftSlavePort);
+  public SpeedControllerGroup left = new SpeedControllerGroup(leftMaster, leftSlave);
+
+  public Victor rightMaster = new Victor(RobotMap.rightMasterPort);
+  public Victor rightSlave = new Victor(RobotMap.rightSlavePort);
+  public SpeedControllerGroup right = new SpeedControllerGroup(rightMaster, rightSlave);
+
+  // DifferentialDrive object
+  // Motor Controllers assigned to differential drive 
+  public DifferentialDrive drive = new DifferentialDrive(leftMaster, rightMaster);
+
+  // Constructor
 
   @Override
   public void initDefaultCommand() {
