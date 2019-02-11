0c95a45d28ce609ad67da47c1a18592f57c57cde
Jeron Artest <jeronsmail@gmail.com>
2019-02-11 14:58:27 -0700

    requires(Robot.driveSubsystem)

--- src/main/java/frc/robot/commands/buttonLBCommand.java
+++ src/main/java/frc/robot/commands/buttonLBCommand.java
@@ -14,6 +14,7 @@ public class buttonLBCommand extends Command {
   public buttonLBCommand() {
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
+    requires(Robot.driveSubsystem);
   }
 
   // Called just before this Command runs the first time
