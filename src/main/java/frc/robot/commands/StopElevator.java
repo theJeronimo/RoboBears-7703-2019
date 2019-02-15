014c049e8e03381572a76b03e04a1f6521db8db1
jeronartest <jeronsmail@gmail.com>
2019-02-15 17:41:06 -0600

    Stop elevator?

--- src/main/java/frc/robot/commands/StopElevator.java
+++ src/main/java/frc/robot/commands/StopElevator.java
@@ -8,11 +8,13 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import frc.robot.Robot;
 
 public class StopElevator extends Command {
   public StopElevator() {
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
+    requires(Robot.armSubsystem);
   }
 
   // Called just before this Command runs the first time
@@ -23,6 +25,7 @@ public class StopElevator extends Command {
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    Robot.armSubsystem.moveElevator(0);
   }
 
   // Make this return true when this Command no longer needs to run execute()
